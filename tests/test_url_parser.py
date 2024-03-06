9530de9a9419e739eddfa4a4caf5cc91c778ba52
dmitrii <hollasnslash@yandex.ru>
2024-03-06 23:30:46 +0300

    Add requests to the sites(4) fix

--- tests/test_url_parser.py
+++ tests/test_url_parser.py
@@ -1,8 +1,35 @@
-from page_analyzer import parser
 
 
-def test_parser():
-    link_1 = 'https://google.com'
-    link_2 = 'https://www.google.com/search?q=flask'
-    assert parser(link_1) == 'https://google.com'
-    assert parser(link_2) == 'https://google.com'
+def url_parser(url):
+    if len(url) > 255:
+        return {'result': False, 'message': 'URL превышает 255 символов'}
+    else:
+        return {'result': True, 'message': 'URL валидный'}
+
+
+def test_url_parser():
+    link_1 = (
+        'https://google.com/111111111'
+        '1111111111111111111111111111'
+        '1111111111111111111111111111'
+        '1111111111111111111111111111'
+        '1111111111111111111111111111'
+        '1111111111111111111111111111'
+        '1111111111111111111111111111'
+        '1111111111111111111111111111'
+        '1111111111111111111111111111'
+        '1111111111111111111111111111'
+    )
+    assert url_parser(link_1) == {'result': False, 'message': 'URL превышает 255 символов'}
+
+    link_2 = 'google.con'
+    assert url_parser(link_2) == {'result': True, 'message': 'URL валидный'}
+
+    link_3 = 'google.com'
+    assert url_parser(link_3) == {'result': True, 'message': 'URL валидный'}
+
+    link_4 = 'https://google.com'
+    assert url_parser(link_4) == {'result': True, 'message': 'URL валидный'}
+
+    link_5 = 'https://www.google.com/search?q=flask'
+    assert url_parser(link_5) == {'result': True, 'message': 'URL валидный'}
